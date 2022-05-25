f1b503d32a45158c010dd1284b863edf89b5ebfa
YottaYocta <chubbean.rq@gmail.com>
2022-05-24 19:03:33 -0500

    added post-processing and modular random object placement

--- game/RandomBuilding.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody
-
-func _ready():
-	randomize()
-	$AnimatedSprite3D.frame = randi() % $AnimatedSprite3D.frames.get_frame_count('default')
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- game/RandomObject.gd
+++ game/RandomObject.gd
@@ -1,8 +1,10 @@
 extends StaticBody
 
 func _ready():
-	randomize()
-	$AnimatedSprite3D.frame = randi() % $AnimatedSprite3D.frames.get_frame_count('default')
+	var objects: AnimatedSprite3D = get_node_or_null('AnimatedSprite3D')
+	if objects:
+		randomize()
+		objects.frame = randi() % objects.frames.get_frame_count('default')
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
